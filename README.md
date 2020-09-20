b899a78a215b38a558a721549150689219de7d52
SushantAI <sushantchatufale@gmail.com>
2020-09-20 09:33:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Recipe Search Engine
+Adapted from Lyrics Search Demo Jina example
+
+Dataset: https://www.kaggle.com/shuyangli94/food-com-recipes-and-user-interactions
+##Command	Description
+pip install -r requirements.txt
+
+python app.py index	-> To index files/data
+
+python app.py search -> To run query on the index
+
+##View in Browser
+cd static
+
+python -m http.server
+
+Open http://0.0.0.0:8000/ in your browser.
+
+##Changes Made
+ranker.yml - Tried using LevenshteinRanker instead of MinRanker
+
+encode.yml - Tried using UniversalSentenceEncoder
+
+craft.yml - sentence length changed from 6 to 3
+
+Add ingredient and steps tags, recipe as text 
+##Errors
+While querying on the index:
+
+netstat -ltnp | grep :65481
+
+sudo kill -9 PID
+
+While running server:
+
+netstat -ltnp | grep :8000
+
+sudo kill -9 PID
+
---
+++ app.py
@@ -0,0 +1,83 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+__version__ = '0.0.1'
+
+import csv
+import os
+import sys
+import itertools
+
+from jina.flow import Flow
+from jina.proto import jina_pb2
+
+
+def config():
+    parallel = 2 if sys.argv[1] == 'index' else 1
+
+    os.environ.setdefault('JINA_MAX_DOCS', '10000')
+    os.environ.setdefault('JINA_PARALLEL', str(parallel))
+    os.environ.setdefault('JINA_SHARDS', str(4))
+    os.environ.setdefault('JINA_WORKSPACE', './workspace')
+    os.makedirs(os.environ['JINA_WORKSPACE'], exist_ok=True)
+    os.environ.setdefault('JINA_PORT', str(65481))
+
+
+def input_fn():
+    recipe_file = os.environ.setdefault('JINA_DATA_PATH', 'input_data/recipes.csv')
+    with open(recipe_file, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in itertools.islice(reader, int(os.environ['JINA_MAX_DOCS'])):
+            if (row[0] == 'name') and (row[5] == 'tags'):
+                continue
+            d = jina_pb2.Document()
+            d.tags['name'] = row[0]
+            d.tags['description'] = row[1]
+            d.tags['ingredients'] = row[4]
+            d.text = row[2]
+            yield d
+
+
+# for index
+def index():
+    f = Flow.load_config('flows/index.yml')
+
+    with f:
+        f.index(input_fn, batch_size=8)
+
+    # for search
+
+
+def search():
+    f = Flow.load_config('flows/query.yml')
+
+    with f:
+        f.block()
+
+
+# for test before put into docker
+def dryrun():
+    f = Flow.load_config('flows/query.yml')
+
+    with f:
+        pass
+
+
+if __name__ == '__main__':
+    #os.environ.setdefault('JINA_MAX_DOCS', '100')
+    #input_fn()
+
+    if len(sys.argv) < 2:
+        print('choose between "index/search/dryrun" mode')
+        exit(1)
+    if sys.argv[1] == 'index':
+        config()
+        index()
+    elif sys.argv[1] == 'search':
+        config()
+        search()
+    elif sys.argv[1] == 'dryrun':
+        config()
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
